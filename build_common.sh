4ba1eb5be3cfe768c309f68a4adc821e17b928c0
MollySophia <mollysophia379@gmail.com>
2022-05-02 16:08:19 +0800

    Add simpleinit submodule It may be useful someday Signed-off-by: MollySophia <mollysophia379@gmail.com>

--- build_common.sh
+++ build_common.sh
@@ -1,3 +1,32 @@
-export PACKAGES_PATH=$PWD/../edk2:$PWD/../edk2-platforms:$PWD
-export WORKSPACE=$PWD/
+for i in "${EDK2}" ./edk2 ../edk2
+do	if [ -n "${i}" ]&&[ -f "${i}/edksetup.sh" ]
+	then	_EDK2="$(realpath "${i}")"
+		break
+	fi
+done
+for i in "${EDK2_PLATFORMS}" ./edk2-platforms ../edk2-platforms
+do	if [ -n "${i}" ]&&[ -d "${i}/Platform" ]
+	then	_EDK2_PLATFORMS="$(realpath "${i}")"
+		break
+	fi
+done
+for i in "${SIMPLE_INIT}" F11/Library/SimpleInit ./simple-init ../simple-init
+do	if [ -n "${i}" ]&&[ -f "${i}/SimpleInit.inc" ]
+	then	_SIMPLE_INIT="$(realpath "${i}")"
+		break
+	fi
+done
+echo "EDK2 Path: ${_EDK2}"
+echo "EDK2_PLATFORMS Path: ${_EDK2_PLATFORMS}"
+export CROSS_COMPILE="${CROSS_COMPILE:-aarch64-linux-gnu-}"
+export GCC5_AARCH64_PREFIX="${CROSS_COMPILE}"
+export CLANG38_AARCH64_PREFIX="${CROSS_COMPILE}"
+export PACKAGES_PATH="$_EDK2:$_EDK2_PLATFORMS:$_SIMPLE_INIT:$PWD"
+export WORKSPACE="${PWD}"
+
 . ../edk2/edksetup.sh
+
+mkdir -p "${_SIMPLE_INIT}/build"
+bash "${_SIMPLE_INIT}/scripts/gen-rootfs-source.sh" \
+	"${_SIMPLE_INIT}" \
+	"${_SIMPLE_INIT}/build"
